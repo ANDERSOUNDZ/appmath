1d411bbc340af822d7da25599e605f5411b330e5
Anderson Chanchay <andersonchanchay@iMac-de-Anderson.local>
2020-09-06 22:01:53 -0500

    fix image 3d & 2D

--- mathapp/views.py
+++ mathapp/views.py
@@ -59,10 +59,11 @@ def appmathView(request):
     return render(request,'grafica/mategraph.html')
 
 def result (request):
-    print('pasa por la funcion')
-    a = int(request.GET['a'])
-    b = int(request.GET['b'])
-    c = int(request.GET['c'])
+    #FIGURA 2D
+    # print('pasa por la funcion')
+    # a = int(request.GET['a'])
+    # b = int(request.GET['b'])
+    # c = int(request.GET['c'])
     # x = np.linspace(10, -10, 100)
     # y = a*(x**2)+b*x+c
     # fig = plt.figure()
@@ -74,7 +75,20 @@ def result (request):
     # ax.spines['top'].set_color('none')
     # ax.xaxis.set_ticks_position('bottom')   
     # ax.yaxis.set_ticks_position('left')
-
+    # print('pasa 2')
+    # buf= io.BytesIO()
+    # fig.savefig(buf, format="png")
+    # buf.seek(0)
+    # string=base64.b64encode(buf.read())
+    # uri= urllib.parse.quote(string)
+    # return render(request, 'grafica/resultado.html',{'data':uri})
+    #FIN FIGURA 2D
+    
+    #FIGURA 3D
+    print('pasa por la funcion')
+    a = int(request.GET['a'])
+    b = int(request.GET['b'])
+    c = int(request.GET['c'])
     #imagen 3d
     X = np.arange(-5, 5, 0.25)
     Y = np.arange(-5, 5, 0.25)
@@ -82,7 +96,6 @@ def result (request):
     x = np.sqrt(X**2 + Y**2)
     y = a*(x**2)+b*x+c
 
-    
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.plot_surface(X, Y, y, rstride=1, cstride=1, cmap=cm.viridis)
@@ -93,45 +106,5 @@ def result (request):
     buf.seek(0)
     string=base64.b64encode(buf.read())
     uri= urllib.parse.quote(string)
-    #return render(request, 'grafica/resultado.html',{'resultado':a,'data':uri})
-    return render(request, 'grafica/resultado.html',{'data':uri})
-
-    #plt.plot(x,y, 'g')
-    #x = int(request.GET['x'])
-
-    #crear x y_ y
-    #fig, ax = plt.subplots()
-
-    #ax.plot([x], [y])
-    #como el comportamiento de la linea, y devuelve x negativo , x positivo, Y negativo , y Y positivo
-    #ax.plot([1, 2, -3, 5], [1, -5, 2, 3]) 
-    #rango
-    # x = np.linspace(-10, 10, 1000)
-    # y=2*(x**2) + a*x - 2
-
-    # x = range(-10, 15)
-    # plt.plot(x, [(i) for i in x])
-    # plt.axhline(0, color="black")
-
-    # plt.xlim(-10, 10)
-    # plt.ylim(-10, 10)
-
-
-
-    # b = int(request.GET['b'])
-    # c = int(request.GET['c'])
-    # p = int(request.GET['p'])
-    # x = np.linspace(-5,5,1)
-    # y = a*(x**2)+b*x+c
-
-    #figura = plt.figure()
-    # figura, ax = plt.subplots()
-    # ax.plot(x, y)
-    # ax = figura.add_subplot(1, 1, 1)
-    # ax.spines['left'].set_position('center')
-    # ax.spines['bottom'].set_position('center')
-    # ax.spines['right'].set_color('none')
-    # ax.spines['top'].set_color('none')
-    # ax.xaxis.set_ticks_position('bottom')   
-    # ax.yaxis.set_ticks_position('left')
-    # plt.plot(x,y, 'g')
+    return render(request,'grafica/resultado.html',{'data':uri})
+    #FIN FIGURA 3D
